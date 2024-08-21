ce982914fd4bbb02e61d8bbb0a1a144e8bcf7d23
Neeraj Kumar <9005042804n@gmail.com>
2024-08-21 17:21:27 +0530

    added model Js file

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -1,80 +1,57 @@
-import { MongoGCPError } from "mongodb";
-import mongoose, {Schema} from "mongoose";
- 
-const userSchema=new Schema({
-    username:{
-        type:String,
-        unique:String,
-        trim:true,
-        lowercase:true,
-        index:true,
-        required:true,
+import mongoose, { Schema } from "mongoose";
 
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    index: true,
+    required: true,
+  },
+  Fullname: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    index: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true,
+  },
+  coverImage: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: [true, "Password is required"],
+  },
+  refreshToken: {
+    type: String,
+    unique: true,
+  },
+  watchHistory: [{
+    type: Schema.Types.ObjectId,
+    ref: "video",
+  }],
+});
 
+const User = mongoose.model("User", userSchema);
 
-    },
-    Fullname:{
-        type:String,
-       
-        trim:true,
-        lowercase:true,
-        index:true,
-        required:true,
-
-
-
-    },
-    email:{
-        type:String,
-        unique:String,
-        trim:true,
-        lowercase:true,
-        required:true,
-    },
-    avatar:{
-        type:String,
-        unique:String,
-        trim:true,
-        lowercase:true,
-        
-        required:true,
-
-
-
-    },
-    coverImage:{
-        type:String,
-        unique:String,
-        trim:true,
-        lowercase:true,
-
-
-
-    },
-    password:{
-        type:String,
-        unique:String,
-        lowercase:true,
-        required:true ,"password is required",
-
-
-
-    },
-    refreshToken:{
-        type:String,
-        unique:String,
-       
-
-
-
-    },
-    watchHistory:{
-        type:Schema.Types.ObjectId,
-        ref:"video"
-    }
-
-})
-
-const User= mongoose.model("User",userSchema)
-
-
+export default User;
